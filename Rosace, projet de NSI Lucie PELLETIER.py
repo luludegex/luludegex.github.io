7c6f03f7f0ccd9d071b110282ae7a5eb629ab3a8
Pelletierlucie <luludegex@gmail.com>
2020-03-05 23:31:19 +0100

    rosace programme

---
+++ Rosace, projet de NSI Lucie PELLETIER.py
@@ -0,0 +1,30 @@
+﻿#etape6:A+B+A+A+B+A+B+A+A+B+A+A+B
+
+from turtle import*
+
+codeGen="AB"
+codeTemp=""
+etapes= 17 # nous appercevons l'apparition d'une rosace complète à partir de 17 itérations
+color("light blue")
+tracer(750,0)
+for nombre in range(etapes -1):
+    codeTemp=""
+    for element in  codeGen:
+     if element =="A":
+        codeTemp=codeTemp+"A+B"
+     else :
+        codeTemp=codeTemp+"A"
+    codeGen=codeTemp
+
+
+
+for element in codeGen:
+    if element=="A":
+        left(10)
+    if element=="B":
+        forward(10)
+    if element=="+":
+        right(10)
+update()
+done()
+
